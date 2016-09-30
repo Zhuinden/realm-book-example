1ce279057a2719da0c7b09ae2872bb99b6dbd7eb
Gabor Varadi <zhuinden@gmail.com>
2016-09-30 07:56:39 +0200

    revise initial data

--- app/src/main/java/com/zhuinden/realmbookexample/application/RealmInitialData.java
+++ app/src/main/java/com/zhuinden/realmbookexample/application/RealmInitialData.java
@@ -11,52 +11,41 @@ import io.realm.Realm;
  * Created by Zhuinden on 2016.08.16..
  */
 public class RealmInitialData implements Realm.Transaction {
-    final List<Book> books = new ArrayList<>();
-
-    public RealmInitialData() {
+    @Override
+    public void execute(Realm realm) {
         Book book = new Book();
+        
         book.setId(1);
         book.setAuthor("Reto Meier");
         book.setTitle("Android 4 Application Development");
         book.setImageUrl("http://api.androidhive.info/images/realm/1.png");
-        books.add(book);
+        realm.insertOrUpdate(book);
 
-        book = new Book();
         book.setId(2);
         book.setAuthor("Itzik Ben-Gan");
         book.setTitle("Microsoft SQL Server 2012 T-SQL Fundamentals");
         book.setImageUrl("http://api.androidhive.info/images/realm/2.png");
-        books.add(book);
+        realm.insertOrUpdate(book);
 
-        book = new Book();
         book.setId(3);
         book.setAuthor("Magnus Lie Hetland");
         book.setTitle("Beginning Python: From Novice To Professional Paperback");
         book.setImageUrl("http://api.androidhive.info/images/realm/3.png");
-        books.add(book);
+        realm.insertOrUpdate(book);
 
-        book = new Book();
         book.setId(4);
         book.setAuthor("Chad Fowler");
         book.setTitle("The Passionate Programmer: Creating a Remarkable Career in Software Development");
         book.setImageUrl("http://api.androidhive.info/images/realm/4.png");
-        books.add(book);
+        realm.insertOrUpdate(book);
 
-        book = new Book();
         book.setId(5);
         book.setAuthor("Yashavant Kanetkar");
         book.setTitle("Written Test Questions In C Programming");
         book.setImageUrl("http://api.androidhive.info/images/realm/5.png");
-        books.add(book);
+        realm.insertOrUpdate(book);
     }
-
-    @Override
-    public void execute(Realm realm) {
-        for(Book b : books) {
-            realm.insertOrUpdate(b);
-        }
-    }
-
+    
     @Override
     public int hashCode() {
         return RealmInitialData.class.hashCode();
